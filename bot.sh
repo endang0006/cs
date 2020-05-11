2541d76aa386cac2ad60f9005d63d5223bc93060
endang0006 <64590296+endang0006@users.noreply.github.com>
2020-05-11 11:35:39 +0700

    Update bot.sh

--- bot.sh
+++ bot.sh
@@ -2,8 +2,54 @@
 #isinomor ganti dengan nomor kalian
 #formatcoin ganti dengan format bot yang kalian inginkan
 #contoh format coin ( btc, bch,ltc,zec,doge )
-python bot.py +isinomor formatcoin && 
-python bot.py +isinomor formatcoin && 
-python bot.py +isinomor formatcoin && 
-python bot.py +isinomor formatcoin && 
-python bot.py +isinomor formatcoin && 
+python bot.py +16286664692 ltc &&
+python bot.py +16286664692 bch &&
+python bot.py +16285004502 ltc &&
+python bot.py +16285004502 bch &&
+python bot.py +16282146155 ltc &&
+python bot.py +16282146155 bch &&
+python bot.py +16282290212 ltc &&
+python bot.py +16282290212 bch &&
+python bot.py +16282385489 ltc &&
+python bot.py +16282385489 bch &&
+python bot.py +18322461818 ltc &&
+python bot.py +18322461818 bch &&
+python bot.py +16234730968 ltc &&
+python bot.py +16234730968 bch &&
+python bot.py +16233836565 ltc &&
+python bot.py +16233836565 bch &&
+python bot.py +16285005976 ltc &&
+python bot.py +16285005976 bch &&
+python bot.py +16234018082 ltc &&
+python bot.py +16234018082 bch &&
+python bot.py +16238008899 ltc &&
+python bot.py +16238008899 bch &&
+python bot.py +17252095200 ltc &&
+python bot.py +17252095200 bch &&
+python bot.py +16239000997 ltc &&
+python bot.py +16239000997 bch &&
+python bot.py +16233836432 ltc &&
+python bot.py +16233836432 bch &&
+python bot.py +16235622401 ltc &&
+python bot.py +16235622401 bch &&
+python bot.py +16237459577 ltc &&
+python bot.py +16237459577 bch &&
+python bot.py +16235621709 ltc &&
+python bot.py +16235621709 bch &&
+python bot.py +16233354036 ltc &&
+python bot.py +16233354036 bch &&
+python bot.py +16232350494 ltc &&
+python bot.py +16232350494 bch &&
+python bot.py +16282154301 ltc &&
+python bot.py +16282154301 bch &&
+python bot.py +16234733388 ltc &&
+python bot.py +16234733388 bch &&
+python bot.py +16232096222 ltc &&
+python bot.py +16232096222 bch &&
+python bot.py +16292225765 ltc &&
+python bot.py +16292225765 bch &&
+python bot.py +16292223878 bch &&
+sh bot.sh
+
+
+
